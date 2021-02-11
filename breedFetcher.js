b4d9f69173b4e19c94caab46e71ca7e61cb18215
Neil Oribello <neil.oribello@gmail.com>
2021-02-11 21:48:29 +0000

    refactored to add in mocha/chai tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,20 @@
 const request = require('request');
 
-const args = process.argv.slice(2);
-const breed = args[0];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-  } else if (!error) {
+const fetchBreedDescription = function(breed, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    if (error) {
+      console.log('error:', error); // Print the error if one occurred
+    }
     const data = JSON.parse(body);
-    console.log(data[0].description);
-  }
-});
+    //if the breed exists
+    if (data[0]) {
+      //null removes the error created in input.js | callback acts as the display
+      callback(null, data[0].description);
+    } else {
+      callback(null, `Can't find the breed ${breed}`);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
